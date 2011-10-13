f875980dd41060dc83247dcc5539d1b4e1df924c
Scanner <scanner@apricot.com>
2011-10-12 21:52:08 -0700

    This little utility might actually kind of work

--- utilities/move_to_subdir_by_year.py
+++ utilities/move_to_subdir_by_year.py
@@ -14,6 +14,8 @@ Except we will keep at least 1,000 in the folder
 #
 import sys
 import mailbox
+import email.utils
+import os
 
 #############################################################################
 #
@@ -39,30 +41,45 @@ def main():
         for msg_key in msgs:
             msg = mbox[msg_key]
             if 'delivery-date' in msg:
-                date = msg['delivery-date']
+                tt = email.utils.parsedate_tz(msg['delivery-date'])
+                date = email.utils.mktime_tz(tt)
+                year = tt[0]
             elif 'date' in msg:
-                date = msg['date']
+                tt = email.utils.parsedate_tz(msg['date'])
+                date = email.utils.mktime_tz(tt)
+                year = tt[0]
             else:
                 date = os.path.getmtime(os.path.join(source_folder,
                                                      str(msg_key)))
-            msg_array.append((date,msg_key))
+                tt = time.gmtime(date)
+                year = tt[0]
+
+            msg_array.append((date,year,msg_key))
 
         msg_array.sort(lambda x,y: x[0] < y[0])
 
         msg_array = msg_array[-1000:]
         subfolder = None
         subfolder_year = None
-        for date, msg_key in msg_array:
+
+        for date, year, msg_key in msg_array:
             msg = mbox[msg_key]
-            year_of_msg = year_of_date(date)
             if subfolder is None:
-                subfolder_year = year_of_msg
-                subfolder = mailbox.MH(os.path.join(source_folder,
-                                                    "%s-%04d" % (os.path.basename(source_folder), year)), create = True)
-            if subfolder and subfolder_year != year_of_msg
-                subfolder_year = year_of_msg
-                subfolder = mailbox.MH(os.path.join(source_folder,
-                                                    "%s-%04d" % (os.path.basename(source_folder), year)), create = True)
+                subfolder_year = year
+                folder_name = "%s-%04d" % \
+                    (os.path.basename(source_folder), year)
+                print "making folder: %s" % folder_name
+                #subfolder = folder_name
+                subfolder = mailbox.MH(os.path.join(source_folder,folder_name),
+                                       create = True)
+            if subfolder and subfolder_year != year:
+                subfolder_year = year
+                folder_name = "%s-%04d" % \
+                    (os.path.basename(source_folder), year)
+                print "making folder: %s" % folder_name
+                # subfolder = folder_name
+                subfolder = mailbox.MH(os.path.join(source_folder,folder_name),
+                                       create = True)
             subfolder.add(msg)
             mbox.remove(msg)
 
