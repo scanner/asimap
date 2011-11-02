22da61dbb3786c41d80b0000b3488652dd744ea5
Scanner <scanner@apricot.com>
2011-11-01 20:26:55 -0700

    Add a FreeBSD etc/rc.d script to startup the asimap server.

---
+++ utils/asimapd.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# $Id$
+#
+# PROVIDE: asimapd_daemon
+# KEYWORD: FreeBSD
+#
+# Start the daemon that will gather temperature data and submit it to Paglo.
+#
+pid_file="/var/run/asimapd.pid"
+
+asimapd_flags=${asimapd_flags-"--debug --ssl_certificate=/etc/ssl/imapscert.pem"}
+
+. /etc/rc.subr
+
+name=asimapd
+
+rcvar=`set_rcvar`
+
+start_cmd=asimapd_start
+stop_cmd=asimapd_stop
+
+export PATH=$PATH:/usr/local/bin
+asimapd_bin=/usr/local/libexec/asimapd.py
+
+asimapd_start() {
+    checkyesno asimapd_enable && echo "Starting asimapd" && \
+        ${asimapd_bin} ${asimapd_flags}
+}
+
+asimapd_stop() {
+    if [ -f ${pid_file} ]
+        then echo "Stopping asimapd" && kill `cat ${pid_file}` && rm ${pid_file}
+    else
+	echo "pid file ${pid_file} does not exist. asimapd not running?"
+    fi
+}
+
+load_rc_config ${name}
+run_rc_command "$1"
